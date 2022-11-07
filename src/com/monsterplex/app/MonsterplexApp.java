89d3fb9276980ddf491b083d33467d8906930752
Hitesh Ahir <ahir.hitesh81@gmail.com>
2022-11-07 14:57:36 -0800

    Created welcome page

--- src/com/monsterplex/app/MonsterplexApp.java
+++ src/com/monsterplex/app/MonsterplexApp.java
@@ -1,4 +1,36 @@
 package com.monsterplex.app;
 
-class MonsterplexApp {
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * Controller - directs flow of an app like user promp,
+ * sending provided inputs to system classes
+ */
+
+public class MonsterplexApp {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void execute() {
+        welcome();
+    }
+
+    private void welcome() {
+        try {
+            File file = new File("MonsterplexArt.txt");
+            Scanner myReader = new Scanner(file);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
+
+
+
+
--- src/com/monsterplex/client/Main.java
+++ src/com/monsterplex/client/Main.java
@@ -1,14 +1,17 @@
 package com.monsterplex.client;
 
 import com.monsterplex.Map;
+import com.monsterplex.app.MonsterplexApp;
 
 import java.io.IOException;
 
 class Main {
 
     public static void main(String[] args) throws IOException {
-        Map testing = new Map();
-        testing.loadMap(1,2);
+        /*Map testing = new Map();
+        testing.loadMap(1,2);*/
+        MonsterplexApp app = new MonsterplexApp();
+        app.execute();
 
     }
 }
