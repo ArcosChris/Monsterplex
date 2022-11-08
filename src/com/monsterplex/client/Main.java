cefe0fc4e719129aacc6f629785b4963a18743ab
Hitesh Ahir <ahir.hitesh81@gmail.com>
2022-11-07 16:17:12 -0800

    Created welcome  updates

--- src/com/monsterplex/client/Main.java
+++ src/com/monsterplex/client/Main.java
@@ -3,12 +3,14 @@ package com.monsterplex.client;
 import com.monsterplex.Monster;
 import com.monsterplex.MonsterType;
 import com.monsterplex.Player;
+import com.monsterplex.app.MonsterplexApp;
 
 class Main {
 
     public static void main(String[] args) {
 
-
+        MonsterplexApp app = new MonsterplexApp();
+        app.execute();
         Player player = new Player("John");
         Monster monster = new Monster(MonsterType.CYCLOPS);
         System.out.println("Current Player Health =" + player.getHealth());
